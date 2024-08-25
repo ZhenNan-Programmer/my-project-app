6d7ace3b8a8fcec2a9aacca913dc1d77ca3ccc67
Zhen Nan <lowzhennan@gmail.com>
2024-08-25 17:33:04 +0800

    Added .github and workflows folder, modified vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // set your base to your folder name
   base: '/ZhenNanProject',
 })
